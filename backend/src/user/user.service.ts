a7364bd1f702a7560ab2f0fe392bc7138222f9d7
DaadouchaSalma <salmadaadoucha@gmail.com>
2025-12-06 23:56:51 +0100

    profile page

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -8,63 +8,96 @@ import { KeycloakAdminService } from 'src/keycloak/keycloak-admin.service';
 
 @Injectable()
 export class UserService {
-    constructor(
-        @InjectRepository(User)
-        private readonly userRepository: Repository<User>,
-        private readonly keycloakAdmin: KeycloakAdminService,
-    ) { }
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    private readonly keycloakAdmin: KeycloakAdminService,
+  ) {}
 
-    async findAllMedecinsAndReceptionnistes(): Promise<User[]> {
-        const users = await this.userRepository.find({
-            where: [
-                { role: 'medecin' },
-                { role: 'receptionniste' },
-            ],
-        });
+  async findAllMedecinsAndReceptionnistes(): Promise<User[]> {
+    const users = await this.userRepository.find({
+      where: [{ role: 'medecin' }, { role: 'receptionniste' }],
+    });
 
-        const fulluser = await Promise.all(
-            users.map(async (p) => {
-                try {
-                    const kcUser = await this.keycloakAdmin.getUserById(p.keycloak_id);
-                    return {
-                        ...p,
-                        firstName: kcUser.firstName,
-                        lastName: kcUser.lastName,
-                        email: kcUser.email,
-                        phoneNumber: kcUser.attributes?.phoneNumber?.[0] || null,
-                        dateNaissance: kcUser.attributes?.dateNaissance?.[0] || null
-                    };
-                } catch (err) {
-                    console.error(`Could not fetch user ${p.keycloak_id}:, err.message`);
-                    return p;
-                }
-            }),
-        );
-
-        return fulluser.map(fulluser => {
-            if (fulluser.role === 'medecin') {
-                const med = new Medecin();
-                Object.assign(med, fulluser);
-                return med;
-            }
-
-            if (fulluser.role === 'receptionniste') {
-                const rec = new Receptionniste();
-                Object.assign(rec, fulluser);
-                return rec;
-            }
-            return fulluser;
-        });
-    }
-
-    async deleteUserById(id: number): Promise<void> {
-        const user = await this.userRepository.findOne({ where: { id } });
-
-        if (!user) {
-            throw new NotFoundException(`User with id ${id} not found`);
+    const fulluser = await Promise.all(
+      users.map(async (p) => {
+        try {
+          const kcUser = await this.keycloakAdmin.getUserById(p.keycloak_id);
+          return {
+            ...p,
+            firstName: kcUser.firstName,
+            lastName: kcUser.lastName,
+            email: kcUser.email,
+            phoneNumber: kcUser.attributes?.phoneNumber?.[0] || null,
+            dateNaissance: kcUser.attributes?.dateNaissance?.[0] || null,
+          };
+        } catch (err) {
+          console.error(`Could not fetch user ${p.keycloak_id}:, err.message`);
+          return p;
         }
+      }),
+    );
 
-        await this.keycloakAdmin.deleteUser(user.keycloak_id);
-        await this.userRepository.delete(id);
+    return fulluser.map((fulluser) => {
+      if (fulluser.role === 'medecin') {
+        const med = new Medecin();
+        Object.assign(med, fulluser);
+        return med;
+      }
+
+      if (fulluser.role === 'receptionniste') {
+        const rec = new Receptionniste();
+        Object.assign(rec, fulluser);
+        return rec;
+      }
+      return fulluser;
+    });
+  }
+
+  async deleteUserById(id: number): Promise<void> {
+    const user = await this.userRepository.findOne({ where: { id } });
+
+    if (!user) {
+      throw new NotFoundException(`User with id ${id} not found`);
     }
+
+    await this.keycloakAdmin.deleteUser(user.keycloak_id);
+    await this.userRepository.delete(id);
+  }
+
+  async getProfile(keycloakId: string): Promise<User> {
+    const user = await this.userRepository.findOne({
+      where: { keycloak_id: keycloakId },
+    });
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    const kcUser = await this.keycloakAdmin.getUserById(keycloakId);
+
+    const merged = {
+      ...user,
+      firstName: kcUser.firstName,
+      lastName: kcUser.lastName,
+      email: kcUser.email,
+      phoneNumber: kcUser.attributes?.phoneNumber?.[0] || null,
+      dateNaissance: kcUser.attributes?.dateNaissance?.[0] || null,
+    };
+
+    // Convert to correct subclass
+    if (merged.role === 'medecin') {
+      const m = new Medecin();
+      Object.assign(m, merged);
+      return m;
+    }
+
+    if (merged.role === 'receptionniste') {
+      const r = new Receptionniste();
+      Object.assign(r, merged);
+      return r;
+    }
+
+    return merged;
+  }
 }
